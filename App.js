6eae9cdac989b43099ad3a15485f41a0dab07b2c
ViniRdS <vinipb2@gmail.com>
2024-04-10 09:28:11 -0400

    Inicio do trabalho

--- App.js
+++ App.js
@@ -1,20 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from "react-native";
+import Header from "./src/components/Header.component";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    const menus = [
+        { menu: "About" },
+        { menu: "Services" },
+        { menu: "Contact" },
+        { menu: "Map" },
+    ];
+
+    return (
+        <View style={styles.container}>
+            <Header title="Main" items={menus} />
+              Só isso por enquanto
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: "#fff",
+        alignItems: "center",
+        justifyContent: "flex-start",
+    },
 });
